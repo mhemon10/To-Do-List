d226ae3198117bd7a65e69aff23f005202daa18b
MH Emon <mhemon@MHs-MacBook-Air.local>
2025-07-30 22:08:28 +0600

    complete style sec

---
+++ app.js
@@ -0,0 +1,14 @@
+let task = [];
+
+const addTask = () => {
+    
+    const taskInput = document.getElementById('taskInput')
+    const text = 
+    
+}
+
+document.getElementById('newTask').addEventListener('click', function (e) {
+    e.preventDefault()
+
+    addTask()
+})
